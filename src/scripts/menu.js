e100e942540d83ddee7f595c04b738d84c3afd09
Hamed Ghaderi <45762462+hamedghaderi@users.noreply.github.com>
2023-05-17 00:54:40 +0300

    Add components

---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('#hamburger').addEventListener('click', () => {
+	document.querySelector('#nav').classList.toggle('hidden')
+	document.querySelector('#nav').classList.toggle('flex')
+})
